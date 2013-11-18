dedc3887c3f5d520fb38518c517d1f66664144cd
tjzaimee <tjzaimee@163.com>
2013-11-18 17:04:27 +0800

    增加tjz.functions.js

---
+++ src/tjz.functions.js
@@ -0,0 +1,12 @@
+/**
+ * author:tjzaimee@163.com
+ * description:JavaScript tool library
+ * date:2013-11-18
+ */
+
+/**
+ * Encapsulated into the trim function to remove trailing spaces string
+ */
+function trim(text){
+	return text.replace(/^\s+|\s+$/g,"");
+}
